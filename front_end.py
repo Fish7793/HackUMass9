48df335f8e9dac49de814bad8572dd3ba63aaddd
NicoleKaldus <nkaldus@gmail.com>
2021-11-06 02:06:01 -0400

    added files for front end

--- front_end.py
+++ front_end.py
@@ -1,1 +1,18 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__ )
+
+@app.route("/login/")
+def login():
+    return ""
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+# print("hello world")
