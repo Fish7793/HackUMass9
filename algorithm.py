7d8dea7de5cfe654faed244b4b9fa42e3ee41d8c
mumtazf <fatim25m@mtholyoke.edu>
2021-11-06 01:30:00 -0400

    mysql trial 1

---
+++ algorithm.py
@@ -0,0 +1,11 @@
+import mysql.connector as mysql
+
+## connecting to the database using 'connect()' method
+## it takes 3 required parameters 'host', 'user', 'passwd'
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "dbms"
+)
+
+print(db) # it will print a connection object if everything is fine
